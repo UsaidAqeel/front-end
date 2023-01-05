c868d09d616b23b4f582babde835cc6fc153edc5
Usaid <usaidaqeel031@gmail.com>
2023-01-05 17:23:17 +0500

    more Work

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "../pages/login";
+import Signup from "../pages/SignUp";
 import Example from "../pages/home";
+import Login from "../pages/Login";
 
 const RoutesCmp = () => {
   return (
     <Router>
       <Routes>
-        <Route path={"/"} element={<Example />}></Route>
+        <Route path="/" element={<Signup />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<Example />}></Route>
       </Routes>
     </Router>
   );
